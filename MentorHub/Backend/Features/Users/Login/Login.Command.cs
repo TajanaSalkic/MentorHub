9537cb62628de9bffd56b778df8353743286aa66
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-19 23:28:43 +0100

    Login command, login handler, login module and validator

---
+++ MentorHub/Backend/Features/Users/Login/Login.Command.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Features.Users.Login
+{
+    public record Command : IRequest<Response>
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; init; } = string.Empty;
+
+        [Required]
+        public string Password { get; init; } = string.Empty;
+    }
+
+    public record Response
+    {
+        public long UserId { get; init; }
+        public string Email { get; init; } = string.Empty;
+        public string Name { get; init; } = string.Empty;
+        public string Role { get; init; } = string.Empty;
+        public string Token { get; init; } = string.Empty;
+    }
+}
--- MentorHub/Backend/Program.cs
+++ MentorHub/Backend/Program.cs
@@ -1,3 +1,12 @@
+using Backend.Database;
+using Carter;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +14,66 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+
+  
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(cfg => {
+        cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    });
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddFluentValidationAutoValidation();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "https://localhost:7236",
+            ValidAudience = "https://localhost:7236",
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("HIUGhdkciwlc16jcopojjOJNHDOlbpsmHCELVPOkcmdJOE"))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,8 +86,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapCarter();
 
 app.Run();
