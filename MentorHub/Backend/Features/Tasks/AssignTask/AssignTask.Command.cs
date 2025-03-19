0a61ae7cb84eb8f48066a424d979d78a148a7f27
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-20 00:43:42 +0100

    assign task (student)

---
+++ MentorHub/Backend/Features/Tasks/AssignTask/AssignTask.Command.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Backend.Features.Tasks.AssignTask
+{
+    public record Command : IRequest<Response>
+    {
+        public int UserID { get; init; }
+        public int TaskID { get; init; }
+        public int ProjectId { get; init; }
+    }
+
+
+    public record Response
+    {
+        public long TaskId { get; init; }
+        public long UserID { get; init; }
+    }
+}
