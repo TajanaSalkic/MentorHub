ea4adbe4988998003ac3d1b204725190d3c52b87
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-20 11:41:55 +0100

    attach a commit link ot a task

---
+++ MentorHub/Backend/Features/Tasks/CommitLinkToTask/CommitLinkToTask.Handler.cs
@@ -0,0 +1,40 @@
+﻿using Backend.Database;
+using Backend.Models;
+using MediatR;
+
+namespace Backend.Features.Tasks.CommitLinkToTask
+{
+    public class Handler : IRequestHandler<Command, Response>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Handler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var commitLink = new CommitLink
+            {
+                Url = request.CommitUrl,
+            };
+
+            _context.CommitLinks.Add(commitLink);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            var taskCommitLink = new Task_CommitLink
+            {
+                TaskId = request.TaskId,
+                CommitLink_ID = commitLink.Id
+            };
+            _context.Task_CommitLinks.Add(taskCommitLink);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new Response
+            {
+                CommitId = commitLink.Id
+            };
+        }
+    }
+}
---
+++ MentorHub/Backend/Features/Tasks/CommitLinkToTask/CommitLinkToTask.Module.cs
@@ -0,0 +1,27 @@
+﻿using Carter;
+using MediatR;
+
+namespace Backend.Features.Tasks.CommitLinkToTask
+{
+    public class LinkCommitsModule : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/tasks/{taskId}/commits", async (
+                long taskId,
+                Command command,
+                IMediator mediator,
+                CancellationToken cancellationToken) =>
+            {
+                command.TaskId = taskId; 
+                var result = await mediator.Send(command, cancellationToken);
+                return Results.Created($"/api/commits/{result.CommitId}", result);
+            })
+            .WithName("LinkCommits")
+            .WithOpenApi()
+            .RequireAuthorization()
+            .Produces<Response>(StatusCodes.Status201Created)
+            .ProducesValidationProblem();
+        }
+    }
+}
