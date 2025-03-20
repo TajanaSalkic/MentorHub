fe8363e6d01e6036628470ccb6f247d9731def51
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-20 14:44:16 +0100

    assign project to a student

---
+++ MentorHub/Backend/Features/Projects/AssignProject/AssignProject.Command.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Backend.Features.Projects.AssignProject
+{
+    public record Command : IRequest<Response>
+    {
+        public int UserID { get; init; }
+        public int ProjectId { get; init; }
+    }
+
+
+    public record Response
+    {
+        public long ProjectId { get; init; }
+        public long UserID { get; init; }
+    }
+}
