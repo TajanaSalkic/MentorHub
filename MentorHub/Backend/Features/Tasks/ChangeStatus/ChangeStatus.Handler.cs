3f168f14b259bd14718194e894ae28f1990591b4
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-20 15:22:45 +0100

    change status for tasks

---
+++ MentorHub/Backend/Features/Tasks/ChangeStatus/ChangeStatus.Handler.cs
@@ -0,0 +1,45 @@
+﻿using Backend.Database;
+using Backend.Models;
+using MediatR;
+using System.Security.Claims;
+
+namespace Backend.Features.Tasks.ChangeStatus
+{
+    public class Handler : IRequestHandler<Command, Response>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public Handler(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            
+
+            Models.Task task = _context.Tasks.Where(x => x.Id.Equals(request.Id)).FirstOrDefault();
+            var taskUpdated = new Models.Task
+            {
+                Title = task.Title,
+                Description = task.Description,
+                StartDate = task.StartDate,
+                EndDate = task.EndDate,
+                Points = task.Points,
+                Status = request.ProjectStatus
+            };
+
+            _context.Update(taskUpdated);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new Response
+            {
+                Id = taskUpdated.Id,
+                Title = taskUpdated.Title,
+                ProjectStatus = taskUpdated.Status.ToString()
+            };
+        }
+    }
+}
