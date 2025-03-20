07ae25170216dd6447575a80973d7c6e0645e7c7
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-20 13:31:43 +0100

    add user to a group (student/mentor)

---
+++ MentorHub/Backend/Features/Users/AddUserToGroup/AddUserToGroup.Handler.cs
@@ -0,0 +1,38 @@
+﻿using Backend.Database;
+using Backend.Models;
+using MediatR;
+
+namespace Backend.Features.Users.AddUserToGroup
+{
+    public class Handler : IRequestHandler<Command, Response>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Handler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var group_user = new Group_User
+            {
+                Group_ID = request.GroupId,
+                User_ID = request.UserId,
+                Mentor = request.Mentor
+            };
+
+            _context.Group_Users.Add(group_user);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new Response
+            {
+                Id = group_user.Id,
+                GroupId = group_user.Group_ID,
+                UserId = group_user.User_ID,
+                Mentor = group_user.Mentor
+            };
+        }
+
+    }
+}
---
+++ MentorHub/Backend/Features/Users/AddUserToGroup/AddUserToGroup.Module.cs
@@ -0,0 +1,26 @@
+﻿using Carter;
+using MediatR;
+
+namespace Backend.Features.Users.AddUserToGroup
+{
+    public class AddUserToGroupModule : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/adduser", async (
+                Command command,
+                IMediator mediator,
+                CancellationToken cancellationToken) =>
+            {
+                var result = await mediator.Send(command, cancellationToken);
+
+                return Results.Created($"/api/adduser/{result.UserId}", result);
+            })
+            .WithName("AddUserToGroup")
+            .WithOpenApi()
+            .RequireAuthorization()
+            .Produces<Response>(StatusCodes.Status201Created)
+            .ProducesValidationProblem();
+        }
+    }
+}
