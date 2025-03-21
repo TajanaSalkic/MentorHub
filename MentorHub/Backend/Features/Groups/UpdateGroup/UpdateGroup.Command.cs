9bbe7e3d66d940626f28156b19e54fb922bb6163
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-21 12:12:16 +0100

    update group

---
+++ MentorHub/Backend/Features/Groups/UpdateGroup/UpdateGroup.Command.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Backend.Features.Groups.UpdateGroup
+{
+    public record Command : IRequest<Response>
+    {
+        public long Id { get; init; }
+        public string Title { get; init; }
+
+    }
+
+    public record Response
+    {
+        public long GroupId { get; init; }
+
+        public string Title { get; init; }
+
+    }
+}
