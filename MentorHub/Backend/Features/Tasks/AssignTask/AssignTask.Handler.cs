942f67d11c163386d434fe88c0f78fbb6b5b90d8
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-21 13:24:47 +0100

    fix on assign task

--- MentorHub/Backend/Features/Tasks/AssignTask/AssignTask.Handler.cs
+++ MentorHub/Backend/Features/Tasks/AssignTask/AssignTask.Handler.cs
@@ -1,6 +1,7 @@
 ﻿using Backend.Database;
 using Backend.Models;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Features.Tasks.AssignTask
 {
@@ -18,24 +19,87 @@ namespace Backend.Features.Tasks.AssignTask
         public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
         {
 
+            var task = await _context.Task_Projects.FirstOrDefaultAsync(x => x.Task_ID == request.TaskID && x.Project_ID == request.ProjectId && x.Creator == false, cancellationToken);
 
-
-            var taskProjectUser = new Task_Project_User
+            if (task != null)
             {
-                User_ID = request.UserID,
-                Project_ID = request.ProjectId,
-                Task_ID = request.TaskID,
-                Creator = false
-            };
+                task.User_ID = request.UserID;
 
-            _context.Task_Projects.Add(taskProjectUser);
-            await _context.SaveChangesAsync(cancellationToken);
+                await _context.SaveChangesAsync(cancellationToken);
 
-            return new Response
+                return new Response
+                {
+                    TaskId = request.TaskID,
+                    UserID = task.User_ID
+                };
+            }
+            else
             {
-                TaskId = request.TaskID,
-                UserID = request.UserID
-            };
+
+                var taskProjectUser = new Task_Project_User
+                {
+                    User_ID = request.UserID,
+                    Project_ID = request.ProjectId,
+                    Task_ID = request.TaskID,
+                    Creator = false
+                };
+
+                _context.Task_Projects.Add(taskProjectUser);
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return new Response
+                {
+                    TaskId = request.TaskID,
+                    UserID = request.UserID
+                };
+
+            }
+               
         }
+
+        /*
+           public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var task = await _context.Task_Projects.FirstOrDefaultAsync(x => x.Project_ID == request.ProjectId && x.Task_ID == null && x.Creator == false, cancellationToken);
+
+
+            if (task!=null)
+            {
+                task.User_ID = request.UserID;
+
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return new Response
+                {
+                    ProjectId = task.Project_ID,
+                    UserID = task.User_ID
+                };
+            }
+            else
+            {
+                var taskProjectUser = new Task_Project_User
+                {
+                    User_ID = request.UserID,
+                    Project_ID = request.ProjectId,
+                    Task_ID = null,
+                    Creator = false
+                };
+
+                _context.Task_Projects.Add(taskProjectUser);
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return new Response
+                {
+                    ProjectId = taskProjectUser.Project_ID,
+                    UserID = taskProjectUser.User_ID
+                };
+            }
+
+
+
+            
+        }
+    }
+         */
     }
 }
