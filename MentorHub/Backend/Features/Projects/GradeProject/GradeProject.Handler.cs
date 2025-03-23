ef8b0b6156051ceb0bd4727864b8bdfc8cf89c66
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-23 19:59:20 +0100

    grade project

---
+++ MentorHub/Backend/Features/Projects/GradeProject/GradeProject.Handler.cs
@@ -0,0 +1,45 @@
+﻿using Backend.Database;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Features.Projects.GradeProject
+{
+    public class Handler : IRequestHandler<Command, Response>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public Handler(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+
+            var task = await _context.Tasks.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+
+            task.Points = request.Points;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new Response
+            {
+                Id = task.Id,
+                Title = task.Title,
+                Description = task.Description,
+                EndDate = task.EndDate,
+                StartDate = task.StartDate,
+                Points = task.Points,
+
+
+            };
+
+
+        }
+
+
+    }
+}
---
+++ MentorHub/Backend/Features/Projects/GradeProject/GradeProject.Module.cs
@@ -0,0 +1,28 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Features.Projects.GradeProject
+{
+    public class GradeProjectModule : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/api/projects/grade", async (
+                [FromBody] Command command,
+                IMediator mediator,
+                CancellationToken cancellationToken) =>
+            {
+
+
+                var result = await mediator.Send(command, cancellationToken);
+                return Results.Ok(result);
+            })
+            .WithName("GradeProject")
+            .WithOpenApi()
+            .RequireAuthorization()
+            .Produces<Response>(StatusCodes.Status200OK)
+            .ProducesValidationProblem();
+        }
+    }
+}
