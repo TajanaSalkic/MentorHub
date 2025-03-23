e0c8aff4eeda6a636a46ae1cf03a66345761d36e
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-23 19:37:41 +0100

    grade task

---
+++ MentorHub/Backend/Features/Tasks/GradeTask/GradeTask.Command.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Backend.Features.Tasks.GradeTask
+{
+    public record Command(long Id) : IRequest<Response>
+    {
+        public double Points { get; init; }
+    }
+
+    public record Response
+    {
+        public long Id { get; init; }
+        public string Title { get; init; }
+        public string Description { get; init; }
+        public DateTime StartDate { get; init; }
+        public DateTime EndDate { get; init; }
+        public double Points { get; init; }
+    }
+}
