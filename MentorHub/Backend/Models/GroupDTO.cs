11c87167fccd528f4e9a32450faed41d2a7ae89c
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-24 00:14:32 +0100

    get all groups depending on the role of user, login fix for a JWT token to have a role, Project and group DTO

---
+++ MentorHub/Backend/Models/GroupDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class GroupDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public List<ProjectDTO> Projects { get; set; }
+
+    }
+}
---
+++ MentorHub/Backend/Models/ProjectDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class ProjectDTO
+    {
+
+        public long ProjectId { get; set; }
+        public string ProjectName { get; set; }
+
+        public long UserId { get; set; }
+    }
+}
