b33b02a2c1ba80b9dd9d7eb8b18bbcf77117ddf8
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-24 12:30:09 +0100

    get group by id

---
+++ MentorHub/Backend/Features/Groups/GetGroupById/GetGroupById.Command.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Models;
+using MediatR;
+
+namespace Backend.Features.Groups.GetGroupById
+{
+    public record Command(long Id) : IRequest<Response>;
+
+    public record Response
+    {
+        public Group Group { get; init; }
+    }
+}
