7076ae0f9a9eaebc7922079210f5e9dfcb658003
Tajana Salkic <tajana.salkic.22@size.ba>
2025-03-24 11:41:35 +0100

    get project by group id (not done)

---
+++ MentorHub/Backend/Features/Projects/GetProjectsByGroupId/GetProjectsByGroupId.Module.cs
@@ -0,0 +1,25 @@
+﻿using Carter;
+using MediatR;
+
+namespace Backend.Features.Projects.GetProjectsByGroupId
+{
+    public class GetProjectsByGroupIdModule : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/projects/groupId/{id:long}", async (
+                long id,
+                IMediator mediator,
+                CancellationToken cancellationToken) =>
+            {
+                var result = await mediator.Send(new Command(id), cancellationToken);
+                return Results.Ok(result);
+            })
+            .WithName(" GetProjectsByGroupId")
+            .WithOpenApi()
+            .RequireAuthorization()
+            .Produces<Response>(StatusCodes.Status201Created)
+            .ProducesValidationProblem();
+        }
+    }
+}
