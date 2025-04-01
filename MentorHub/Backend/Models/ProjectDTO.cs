50482377dc2c07bda19842f597fae69eeb2452a5
Tajana Salkic <tajana.salkic.22@size.ba>
2025-04-01 14:26:45 +0200

    fix on response on get project by id

--- MentorHub/Backend/Models/ProjectDTO.cs
+++ MentorHub/Backend/Models/ProjectDTO.cs
@@ -3,11 +3,28 @@
     public class ProjectDTO
     {
 
-        public long ProjectId { get; set; }
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public ProjectStatus Status { get; set; }
+
+        public double Points { get; set; }
+
+        public long TasksOnHold { get; set; }
+
+        public long TasksPlanning { get; set; }
+
+        public long TasksActive { get; set; }
+        public long TasksDone { get; set; }
+
+
+       /* public long ProjectId { get; set; }
         public string ProjectName { get; set; }
 
         public long UserId { get; set; }
 
-        public List<Models.Task> Tasks { get; set; }
+        public List<Models.Task> Tasks { get; set; }*/
     }
 }
