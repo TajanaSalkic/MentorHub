a27baa299f97c651e17fc914fffa56b3d9bcf360
Tajana Salkic <tajana.salkic.22@size.ba>
2025-05-06 23:57:25 +0200

    navbar, create project page, dashboard, homepage

--- MentorHub/frontend/src/app/app.routes.ts
+++ MentorHub/frontend/src/app/app.routes.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule} from '@angular/router';
 import { LoginComponent } from '../pages/login/login.component';
 import { RegisterComponent } from '../pages/register/register.component';
-import { HomePageComponent } from '../pages/home-page/home-page.component'; // Import the new component
+import { HomePageComponent } from '../pages/home-page/home-page.component'; 
+import { ProjectDashboardComponent } from '../pages/project-dashboard/project-dashboard.component';
+import { CreateProjectComponent } from '../pages/create-project/create-project.component';
 
 
 export const routes: Routes = [
@@ -11,6 +13,8 @@ export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'home', component: HomePageComponent }, 
+    { path: 'project/:id', component: ProjectDashboardComponent },
+    { path: 'create-project', component: CreateProjectComponent },
 
 ];
 
